dc4061c11de26607da55c086706a034056aa7c44
Evgeny V. Bogodukhov <b@evbg.ru>
2019-10-06 22:46:12 +0400

    flake8

--- bitfxrequest/tests/test_get_rates.py
+++ bitfxrequest/tests/test_get_rates.py
@@ -6,7 +6,7 @@ from bitfxrequest import get_rates
 
 def test_get_rates_01():
     rates = zip(range(3), get_rates())
-    for n, rate in rates:
+    for _n, rate in rates:
         assert len(rate) == 4
         assert type(rate[1]) is datetime
         assert type(rate[2]) is float
